2ecc6974fd80f79f3b9a78c8b2af50cfe937dc60
Jeff Senkevich <jeff@senkevich.com>
2024-02-15 21:30:00 -0500

    Initial commit

---
+++ src/main/java/com/example/javajunction/modulith/JavaJunctionModulithApplication.java
@@ -0,0 +1,13 @@
+package com.example.javajunction.modulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJunctionModulithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJunctionModulithApplication.class, args);
+	}
+
+}
